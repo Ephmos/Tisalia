13ce5d06f9197809e95146ea33a3d4afcda225af
Ephmos <arefokmido@protonmail.com>
2026-01-28 23:21:00 +0100

    SDE

--- scripts/enemy_plant.gd
+++ scripts/enemy_plant.gd
@@ -4,19 +4,15 @@ var speed = 40
 var chase = false
 var player = null
 
-
-func _physics_process(delta):
+func _physics_process(_delta):
 	if chase:
 		position += (player.position -position)/speed
 		move_and_collide(Vector2(0,0)) 
-		
-
+	
 func _on_detection_area_body_entered(body):
 	player = body
 	chase = true
-
-
-func _on_detection_area_body_exited(body):
+	
+func _on_detection_area_body_exited(_body):
 	player = null
 	chase = false
-	
