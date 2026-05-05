227b4e56f1aa9fa05bc43eaaacbdc76763176f54
Klugy <jaencorverajavier@gmail.com>
2026-05-05 11:35:59 +0200

    Revert "bala"
    This reverts commit cb575ca2fdc1c3e54ee39b45708b7f4c6e2f8883.

--- vampire/bullet.gd
+++ vampire/bullet.gd
@@ -1,28 +1,18 @@
 extends Area2D
 
-# STATS
-
-@export var speed: float = 200.0
-@export var damage: int = 1
-
-# ESTADOS
-
+# Velocidad y daño configurables desde el vampiro al instanciar
+var speed: float = 200.0
+var damage: int = 1
 var direction: Vector2 = Vector2.ZERO
-
-# MAIN
 
 func _physics_process(delta: float) -> void:
 	position += direction * speed * delta
 
-# IMPACTO (bala → jugador)
-
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		print("[BULLET] Impacto con: ", body.name)
+	if body.has_method("take_damage"):
 		body.take_damage(damage)
-		queue_free()
+	queue_free()
 
-# FUERA DE PANTALLA
-
+# Eliminar la bala si sale de la pantalla
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
