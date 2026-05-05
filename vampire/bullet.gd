cb575ca2fdc1c3e54ee39b45708b7f4c6e2f8883
Klugy <jaencorverajavier@gmail.com>
2026-05-05 11:16:32 +0200

    bala

--- vampire/bullet.gd
+++ vampire/bullet.gd
@@ -1,18 +1,28 @@
 extends Area2D
 
-# Velocidad y daño configurables desde el vampiro al instanciar
-var speed: float = 200.0
-var damage: int = 1
+# STATS
+
+@export var speed: float = 200.0
+@export var damage: int = 1
+
+# ESTADOS
+
 var direction: Vector2 = Vector2.ZERO
+
+# MAIN
 
 func _physics_process(delta: float) -> void:
 	position += direction * speed * delta
 
-func _on_body_entered(body: Node2D) -> void:
-	if body.has_method("take_damage"):
-		body.take_damage(damage)
-	queue_free()
+# IMPACTO (bala → jugador)
 
-# Eliminar la bala si sale de la pantalla
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		print("[BULLET] Impacto con: ", body.name)
+		body.take_damage(damage)
+		queue_free()
+
+# FUERA DE PANTALLA
+
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
